ca2e0fe9d1688cc9cbbf9ce63e0c1c26530d9ed5
ritomiguelEsb <50515@aebatalha.ccems.pt>
2022-11-21 18:05:36 +0000

    AW

---
+++ DecoderCipher/DecoderCipher/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DecoderCipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string finalText = "";
+        int loop = 0;
+        Random random;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            finalText = "";
+            random = new Random();
+            int change;
+            try
+            {
+                change = Convert.ToInt32(textBox2.Text);
+                change = -change + 26;
+            }
+            catch
+            {
+                change = 1;
+            }
+            for (int i = 0; i < change; i++)
+            {
+                if (finalText== "")
+                {
+                    foreach (char letterfor in textBox1.Text)
+                    {
+                        change_Letter(letterfor);
+                    }
+                }
+                else
+                {
+                    string textToChange = finalText;
+                    finalText = "";
+                    foreach (char letterfor in textToChange)
+                    {
+                        change_Letter(letterfor);
+                    }
+                }
+            }
+            timer1.Enabled = true;
+           
+        }
+
+        private void change_Letter(char letterfor)
+        {
+
+                int letter = char.ToLower(letterfor);
+                char finalLetter;
+                if(!char.IsLetter(letterfor))
+                {
+                    finalText += (char)32;
+                    return;
+                }
+                if (letter + 1 > 122)
+                {
+                    finalLetter = (char)(letter - 25);
+                }
+                else
+                {
+                    finalLetter = (char)(letter + 1);
+                }
+                if (char.IsUpper(letterfor))
+                {
+                    finalText += char.ToUpper(finalLetter);
+                }
+                else
+                {
+                    finalText += finalLetter;
+
+                }
+                
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {;
+            
+            if(loop < 100)
+            {
+                textBox3.Clear();
+                foreach(char rand in textBox1.Text)
+                {
+                    int num = random.Next(33, 126);
+                    textBox3.Text += (char)(num);
+                }
+                loop++;
+            }
+            else
+            {
+                int num = random.Next(33, 126);
+                timer1.Enabled = false;
+                textBox3.Clear();
+                textBox3.Text = finalText;
+                loop = 0;
+            }
+        }
+    }
+}

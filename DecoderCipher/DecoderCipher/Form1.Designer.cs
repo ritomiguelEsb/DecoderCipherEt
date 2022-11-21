d55ba72d0f5146ff8a69a35c5765147308e17b07
ritomiguelEsb <50515@aebatalha.ccems.pt>
2022-11-21 21:27:32 +0000

    fgsdfg

--- DecoderCipher/DecoderCipher/Form1.Designer.cs
+++ DecoderCipher/DecoderCipher/Form1.Designer.cs
@@ -76,27 +76,19 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 62);
             this.button1.TabIndex = 3;
-
-            this.button1.Text = "Converter";
-            this.button1.UseVisualStyleBackColor = false;
-
             this.button1.Text = "Decodificar";
             this.button1.UseVisualStyleBackColor = true;
-
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // progressBar1
             // 
-
             this.progressBar1.BackColor = System.Drawing.Color.PeachPuff;
             this.progressBar1.Location = new System.Drawing.Point(32, 261);
-
-            this.progressBar1.Location = new System.Drawing.Point(90, 261);
             this.progressBar1.Maximum = 500;
-
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(492, 43);
             this.progressBar1.TabIndex = 4;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
             // 
             // timer1
             // 
